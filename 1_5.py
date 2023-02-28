1b2c56ea9fecc610c6259650a7c893108cd64af8
Christos Perchanidis <pmchrist@protonmail.com>
2023-02-28 17:04:23 +0100

    Reformatted a little

--- 1_5.py
+++ 1_5.py
@@ -15,15 +15,17 @@ print(pairs_ri.head())
 
 # 1_5
 
-df_spread_trade = pairs_price.copy()
+df_spread_trade = pd.DataFrame()
+name_security_A = "VW"              # Pricier Asset
+name_security_B = "VW_PREF"         # Cheaper Asset
 # Find Relative Spread
-df_spread_trade["spread_APMOLLER"] = (pairs_price["APMOLLER_B"] - pairs_price["APMOLLER_A"]) / pairs_price["APMOLLER_A"]
+df_spread_trade["spread"] = (pairs_price[name_security_A] - pairs_price[name_security_B]) / pairs_price[name_security_B]
 # Rolling 20-day, Spread Average and Volatility
 window = 20
-df_spread_trade["spread_APMOLLER_rolling_mean"] = df_spread_trade["spread_APMOLLER"].rolling(window).mean()
-df_spread_trade["spread_APMOLLER_rolling_std"] = df_spread_trade["spread_APMOLLER"].rolling(window).std()
+df_spread_trade["spread_rolling_mean"] = df_spread_trade["spread"].rolling(window).mean()
+df_spread_trade["spread_rolling_std"] = df_spread_trade["spread"].rolling(window).std()
 # Trading Based on Z-Score
-df_spread_trade["spread_APMOLLER_rolling_z_score"] = (df_spread_trade["spread_APMOLLER"] - df_spread_trade["spread_APMOLLER_rolling_mean"]) / df_spread_trade["spread_APMOLLER_rolling_std"]
+df_spread_trade["spread_rolling_z_score"] = (df_spread_trade["spread"] - df_spread_trade["spread_rolling_mean"]) / df_spread_trade["spread_rolling_std"]
 
 # Calculating Trades
 df_spread_trade["Trade"] = np.zeros(len(df_spread_trade))
@@ -36,32 +38,32 @@ for i in range(window, len(pairs_price)):
     elif in_short:
         df_spread_trade["Trade"][i] = -1
     # Enter Trade
-    if df_spread_trade["spread_APMOLLER_rolling_z_score"][i-1] > 2 and not in_long:      # Go Long
+    if df_spread_trade["spread_rolling_z_score"][i-1] > 2 and not in_long:      # Go Long
         df_spread_trade["Trade"][i] = 1
         in_long = True
-    elif df_spread_trade["spread_APMOLLER_rolling_z_score"][i-1] < -2 and not in_short:   # Go Short
+    elif df_spread_trade["spread_rolling_z_score"][i-1] < -2 and not in_short:   # Go Short
         df_spread_trade["Trade"][i] = -1
         in_short = True
     # Exit Trade
-    if df_spread_trade["spread_APMOLLER_rolling_z_score"][i-1] < 0 and in_long:      # Stop Long
-        df_spread_trade["Trade"][i] = 5
+    if df_spread_trade["spread_rolling_z_score"][i-1] < 0 and in_long:      # Stop Long
+        df_spread_trade["Trade"][i] = 0
         in_long = False
-    elif df_spread_trade["spread_APMOLLER_rolling_z_score"][i-1] > 0 and in_short:   # Stop Short
-        df_spread_trade["Trade"][i] = -5
+    elif df_spread_trade["spread_rolling_z_score"][i-1] > 0 and in_short:   # Stop Short
+        df_spread_trade["Trade"][i] = 0
         in_short = False
 
 df_spread_trade["Returns"] = np.zeros(len(df_spread_trade))
 for i in range(len(pairs_price)):
-    if (df_spread_trade["Trade"][i] > 0):
-        df_spread_trade["Returns"][i] = (pairs_price["APMOLLER_B"][i] - pairs_price["APMOLLER_B"][i-1]) + (pairs_price["APMOLLER_A"][i-1] - pairs_price["APMOLLER_A"][i])
-    elif (df_spread_trade["Trade"][i] < 0):
-        df_spread_trade["Returns"][i] = (pairs_price["APMOLLER_B"][i-1] - pairs_price["APMOLLER_B"][i]) + (pairs_price["APMOLLER_A"][i] - pairs_price["APMOLLER_A"][i-1])
+    if (df_spread_trade["Trade"][i] > 0):       # We are Short First Security, Long Second
+        df_spread_trade["Returns"][i] = (pairs_price[name_security_A][i-1] - pairs_price[name_security_A][i])/pairs_price[name_security_A][i] + (pairs_price[name_security_B][i] - pairs_price[name_security_B][i-1])/pairs_price[name_security_B][i-1]
+    elif (df_spread_trade["Trade"][i] < 0):     # We are Long First Security, Short Second
+        df_spread_trade["Returns"][i] = (pairs_price[name_security_A][i] - pairs_price[name_security_A][i-1])/pairs_price[name_security_B][i-1] + (pairs_price[name_security_B][i-1] - pairs_price[name_security_B][i])/pairs_price[name_security_B][i]
 
 returns_long = 0
 returns_short = 0
 for i in range(len(pairs_price)):
-    if (df_spread_trade["Trade"][i] == 5): returns_long+=df_spread_trade["Returns"][i]
-    elif (df_spread_trade["Trade"][i] == -5): returns_short+=df_spread_trade["Returns"][i]
+    if (df_spread_trade["Trade"][i] > 0): returns_long+=df_spread_trade["Returns"][i]
+    elif (df_spread_trade["Trade"][i] < 0): returns_short+=df_spread_trade["Returns"][i]
 print("Returns Long: ", returns_long)
 print("Returns Short: ", returns_short)
 
